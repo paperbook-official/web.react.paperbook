c633dfc8c4551b0618ae507d99ab1e6edf98623f
Michell Algarra <michell.algarra@gmail.com>
2021-03-11 22:55:11 -0300

    feat(styles/themes): created base themes with initial configurations

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from 'styled-components';
+
+/**
+ * The app's main light theme.
+ */
+export const light: DefaultTheme = {
+    title: 'light',
+    colors: {
+        defaultBlue: '',
+        defaultGrey: '',
+        defaultGreyBlue: '',
+        defaultHighlightGreyBlue: ''
+    }
+};
