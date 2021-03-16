ba1563cc432d991ded2170e92e2560b025c61caa
Michell Algarra <michell.algarra@gmail.com>
2021-03-15 22:17:19 -0300

    feat(router): implemented the base app routes

---
+++ src/router/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+
+import { isAuthenticated } from '../services/auth';
+
+import Home from '../pages/Home';
+
+interface PrivateRouteProps {
+    component: React.FC;
+    path: string;
+    exact?: boolean;
+}
+
+const PrivateRoute: React.FC<PrivateRouteProps> = (
+    props: PrivateRouteProps
+): JSX.Element =>
+    isAuthenticated() ? (
+        <Route
+            path={props.path}
+            exact={props.exact}
+            component={props.component}
+        />
+    ) : (
+        <Redirect to="/" />
+    );
+
+const Routes = (): JSX.Element => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signup" component={() => <h1>Sign Up</h1>} />
+            <Route exact path="/login" component={() => <h1>Log In</h1>} />
+            <Route exact path="/cart" component={() => <h1>Cart</h1>} />
+            <PrivateRoute path="/private" component={() => <h1>Private</h1>} />
+            <Route path="*" component={() => <h1>Page Not Found</h1>} />
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Routes;
