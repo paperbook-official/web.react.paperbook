86db0b27ec1db3a551d13580b2ea278a97354c69
Michell Algarra <michell.algarra@gmail.com>
2021-03-17 00:07:40 -0300

    feat(components/SocialMedia): created a component to be the default social media redirect buttons

---
+++ src/components/atoms/SocialMedia/index.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+
+import { useTheme } from 'styled-components';
+
+import { ReactComponent as FacebookIcon } from '../../../assets/icons/facebook.svg';
+import { ReactComponent as InstagramIcon } from '../../../assets/icons/instagram.svg';
+import { ReactComponent as TwitterIcon } from '../../../assets/icons/twitter.svg';
+
+import { Container } from './styles';
+
+interface SocialMediaProps {
+    style?: React.CSSProperties;
+}
+
+const SocialMedia: React.FC<SocialMediaProps> = ({
+    style
+}: SocialMediaProps): JSX.Element => {
+    const theme = useTheme();
+
+    const [facebookColor, setFacebookColor] = useState(
+        theme.colors.defaultLightGrey
+    );
+    const [twitterColor, setTwitterColor] = useState(
+        theme.colors.defaultLightGrey
+    );
+    const [instagramColor, setInstagramColor] = useState(
+        theme.colors.defaultLightGrey
+    );
+
+    const iconSize = 30;
+
+    const handleFacebookClick = (): void => {
+        window.open('https://www.facebook.com', '_blank');
+    };
+
+    const handleTwitterClick = (): void => {
+        window.open('https://www.twitter.com', '_blank');
+    };
+
+    const handleInstagramClick = (): void => {
+        window.open('https://www.instagram.com', '_blank');
+    };
+
+    return (
+        <Container style={style}>
+            <FacebookIcon
+                onMouseEnter={() => setFacebookColor('#4267B2')}
+                onMouseLeave={() =>
+                    setFacebookColor(theme.colors.defaultLightGrey)
+                }
+                onClick={handleFacebookClick}
+                style={{ cursor: 'pointer', transition: 'all 0.2s' }}
+                width={`${iconSize}px`}
+                height={`${iconSize}px`}
+                color={facebookColor}
+            />
+            <TwitterIcon
+                onMouseEnter={() => setTwitterColor('#1DA1F2')}
+                onMouseLeave={() =>
+                    setTwitterColor(theme.colors.defaultLightGrey)
+                }
+                onClick={handleTwitterClick}
+                style={{ cursor: 'pointer', transition: 'all 0.2s' }}
+                width={`${iconSize}px`}
+                height={`${iconSize}px`}
+                color={twitterColor}
+            />
+            <InstagramIcon
+                onMouseEnter={() => setInstagramColor('#FFDC80')}
+                onMouseLeave={() =>
+                    setInstagramColor(theme.colors.defaultLightGrey)
+                }
+                onClick={handleInstagramClick}
+                style={{ cursor: 'pointer', transition: 'all 0.2s' }}
+                width={`${iconSize}px`}
+                height={`${iconSize}px`}
+                color={instagramColor}
+            />
+        </Container>
+    );
+};
+
+export default SocialMedia;
---
+++ src/components/atoms/SocialMedia/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    position: absolute;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    width: 116px;
+`;
