29e661d61e5a9e8e4543cfa58ca3660a9cc3ef8b
Michell Algarra <michell.algarra@gmail.com>
2021-03-18 00:00:10 -0300

    feat(models): created a folder to keep all enums and payload/proxies interfaces

---
+++ src/models/enums/cookies.ts
@@ -0,0 +1,5 @@
+export enum CookiesEnum {
+    TOKEN_KEY = 'paperbook-access-token',
+    ACCESS_KEY = 'paperbook-access',
+    CART_AMOUNT_KEY = 'paperbook-cartamount'
+}
---
+++ src/models/payloads/user.ts
@@ -0,0 +1,14 @@
+export interface AuthPayload {
+    email: string;
+    password: string;
+}
+
+export interface SignUpPayload {
+    name: string;
+    lastName: string;
+    email: string;
+    password: string;
+    cpf: string;
+    roles: string;
+    phone: string;
+}
