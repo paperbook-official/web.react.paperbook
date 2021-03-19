42f08f0396de9cac84b897bf94b5e879d75660fa
Michell Algarra <michell.algarra@gmail.com>
2021-03-19 00:13:44 -0300

    feat(models/proxies/address): created the address proxy

---
+++ src/models/proxies/address.ts
@@ -0,0 +1,17 @@
+import { UserProxy } from './user';
+
+export interface AddressProxy {
+    id: number;
+    createdAt: string;
+    updatedAt: string;
+    isActive: boolean;
+    cep: string;
+    street: string;
+    houseNumber: number;
+    complement: string;
+    district: string;
+    city: string;
+    state: string;
+    userId: number;
+    user: UserProxy;
+}
