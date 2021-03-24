f30f64fb49b2768cbe445a911975fe1a589cc6e1
Michell Algarra <michell.algarra@gmail.com>
2021-03-24 16:17:07 -0300

    feat(pages/Home/index.tsx): added almost all product getting methods to home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
-import { ProductProxy } from '../../models/proxies/product';
+import { ManyProductProxy } from '../../models/proxies/product';
+
+import { useProduct } from '../../hooks/useProduct';
 
 import SocialMedia from '../../components/atoms/SocialMedia';
 import CategoriesBar from '../../components/molecules/CategoriesBar';
@@ -30,13 +32,20 @@ import {
 
 const Home: React.FC = (): JSX.Element => {
     const theme = useTheme();
+    const {
+        getProductsByPrice,
+        getProductsOnSale,
+        getInterestFree,
+        getRecentProducts,
+        getMostBought
+    } = useProduct();
 
     const prices: number[] = [10, 20, 30, 40, 50];
 
     const [headerPosition, setHeaderPosition] = useState(-100);
     const [contentOpacity, setContentOpacity] = useState(1);
     const [scrollDownOpacity, setScrollDownOpacity] = useState(1);
-    const [randomPrice] = useState(formatPrice(getRandom(prices)));
+    const [randomPrice] = useState(getRandom(prices));
 
     useEffect(() => {
         if (window.pageYOffset >= 150) setHeaderPosition(0);
@@ -120,180 +129,49 @@ const Home: React.FC = (): JSX.Element => {
         addresses: []
     };
 
-    const products = [
-        {
-            id: 1,
-            createdAt: new Date().toISOString(),
-            updatedAt: new Date().toISOString(),
-            isActive: true,
-            name: 'The Science of Selling',
-            description: 'bla bla bla',
-            fullPrice: 89.9,
-            installmentPrice: 89.9,
-            installmentAmount: 6,
-            discountAmount: 0.2,
-            stockAmount: 978,
-            userId: user.id,
-            user: user
-        },
-        {
-            id: 2,
-            createdAt: new Date().toISOString(),
-            updatedAt: new Date().toISOString(),
-            isActive: true,
-            name: 'Harry Potter e as Relíquias da Morte',
-            description: 'bla bla bla',
-            fullPrice: 44.9,
-            installmentPrice: 50.0,
-            installmentAmount: 5,
-            discountAmount: 0,
-            stockAmount: 978,
-            userId: user.id,
-            user: user
-        },
-        {
-            id: 3,
-            createdAt: new Date().toISOString(),
-            updatedAt: new Date().toISOString(),
-            isActive: true,
-            name: 'Clean Code',
-            description: 'bla bla bla',
-            fullPrice: 39.9,
-            installmentPrice: 39.9,
-            installmentAmount: 4,
-            discountAmount: 0,
-            stockAmount: 978,
-            userId: user.id,
-            user: user
-        },
-        {
-            id: 4,
-            createdAt: new Date().toISOString(),
-            updatedAt: new Date().toISOString(),
-            isActive: true,
-            name: 'Crepúsculo: Amanhecer',
-            description: 'bla bla bla',
-            fullPrice: 79.9,
-            installmentPrice: 89.9,
-            installmentAmount: 8,
-            discountAmount: 0.2,
-            stockAmount: 978,
-            userId: user.id,
-            user: user
-        },
-        {
-            id: 5,
-            createdAt: new Date().toISOString(),
-            updatedAt: new Date().toISOString(),
-            isActive: true,
-            name: 'JAVA',
-            description: 'bla bla bla',
-            fullPrice: 89.9,
-            installmentPrice: 89.9,
-            installmentAmount: 6,
-            discountAmount: 0.2,
-            stockAmount: 978,
-            userId: user.id,
-            user: user
-        },
-        {
-            id: 6,
-            createdAt: new Date().toISOString(),
-            updatedAt: new Date().toISOString(),
-            isActive: true,
-            name: 'Javascript: Web in your hands',
-            description: 'bla bla bla',
-            fullPrice: 89.9,
-            installmentPrice: 89.9,
-            installmentAmount: 6,
-            discountAmount: 0.2,
-            stockAmount: 978,
-            userId: user.id,
-            user: user
-        },
-        {
-            id: 7,
-            createdAt: new Date().toISOString(),
-            updatedAt: new Date().toISOString(),
-            isActive: true,
-            name: 'The Science of Selling',
-            description: 'bla bla bla',
-            fullPrice: 89.9,
-            installmentPrice: 89.9,
-            installmentAmount: 6,
-            discountAmount: 0.2,
-            stockAmount: 978,
-            userId: user.id,
-            user: user
-        },
-        {
-            id: 8,
-            createdAt: new Date().toISOString(),
-            updatedAt: new Date().toISOString(),
-            isActive: true,
-            name: 'The of Selling',
-            description: 'bla bla bla',
-            fullPrice: 89.9,
-            installmentPrice: 89.9,
-            installmentAmount: 6,
-            discountAmount: 0.2,
-            stockAmount: 978,
-            userId: user.id,
-            user: user
-        },
-        {
-            id: 9,
-            createdAt: new Date().toISOString(),
-            updatedAt: new Date().toISOString(),
-            isActive: true,
-            name: 'The Selling',
-            description: 'bla bla bla',
-            fullPrice: 89.9,
-            installmentPrice: 89.9,
-            installmentAmount: 6,
-            discountAmount: 0.2,
-            stockAmount: 978,
-            userId: user.id,
-            user: user
-        },
-        {
-            id: 10,
-            createdAt: new Date().toISOString(),
-            updatedAt: new Date().toISOString(),
-            isActive: true,
-            name: 'The Science of',
-            description: 'bla bla bla',
-            fullPrice: 89.9,
-            installmentPrice: 89.9,
-            installmentAmount: 6,
-            discountAmount: 0.2,
-            stockAmount: 978,
-            userId: user.id,
-            user: user
-        },
-        {
-            id: 11,
-            createdAt: new Date().toISOString(),
-            updatedAt: new Date().toISOString(),
-            isActive: true,
-            name: 'Third',
-            description: 'bla bla bla',
-            fullPrice: 89.9,
-            installmentPrice: 89.9,
-            installmentAmount: 6,
-            discountAmount: 0.2,
-            stockAmount: 978,
-            userId: user.id,
-            user: user
-        }
-    ];
+    const product = {
+        id: 1,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+        isActive: true,
+        name: 'The Science of Selling',
+        description: 'bla bla bla',
+        fullPrice: 89.9,
+        installmentPrice: 89.9,
+        installmentAmount: 6,
+        discountAmount: 0.2,
+        stockAmount: 978,
+        userId: user.id,
+        user: user
+    };
 
-    const getProductsByPrice = (
+    const getProductsByTopic = async (
+        itemsPerPage: number,
+        page: number,
+        request: (
+            page: number,
+            offset: number,
+            itemsPerPage: number,
+            orderBy?: string[]
+        ) => Promise<ManyProductProxy>
+    ): Promise<ManyProductProxy> => {
+        const offset = 0;
+        const data = await request(page, offset, itemsPerPage);
+        return data;
+    };
+
+    const getProductsByPriceLocal = async (
         itemsPerPage: number,
         page: number
-    ): ProductProxy[] => {
-        const offset = (page - 1) * itemsPerPage;
-        return products.slice(offset, offset + itemsPerPage);
+    ): Promise<ManyProductProxy> => {
+        const offset = 0;
+        const data = await getProductsByPrice(
+            randomPrice,
+            page,
+            offset,
+            itemsPerPage
+        );
+        return data;
     };
 
     return (
@@ -330,7 +208,7 @@ const Home: React.FC = (): JSX.Element => {
                 <CustomProductCard
                     style={{ opacity: contentOpacity }}
                     onClick={console.log}
-                    product={products[0]}
+                    product={product}
                     image="https://i1.wp.com/sellingsherpa.com/wp-content/uploads/2021/01/science-of-selling.png?fit=2380%2C700&ssl=1"
                 />
                 <ScrollDown
@@ -349,28 +227,36 @@ const Home: React.FC = (): JSX.Element => {
             <SecondPageContainer>
                 <ProductFlatList
                     style={{ marginTop: 120 }}
-                    topicTitle={`Por menos de R$ ${randomPrice}`}
-                    request={getProductsByPrice}
+                    topicTitle={`Por menos de R$ ${formatPrice(randomPrice)}`}
+                    request={getProductsByPriceLocal}
                     onProductClick={(product) => console.log(product)}
                 />
                 <ProductFlatList
                     topicTitle={`Ofertas`}
-                    request={getProductsByPrice}
+                    request={(i: number, p: number) =>
+                        getProductsByTopic(i, p, getProductsOnSale)
+                    }
                     onProductClick={(product) => console.log(product)}
                 />
                 <ProductFlatList
                     topicTitle={`Parcelamento sem juros`}
-                    request={getProductsByPrice}
+                    request={(i: number, p: number) =>
+                        getProductsByTopic(i, p, getInterestFree)
+                    }
                     onProductClick={(product) => console.log(product)}
                 />
                 <ProductFlatList
                     topicTitle={`Adicionados recentemente`}
-                    request={getProductsByPrice}
+                    request={(i: number, p: number) =>
+                        getProductsByTopic(i, p, getRecentProducts)
+                    }
                     onProductClick={(product) => console.log(product)}
                 />
                 <ProductFlatList
                     topicTitle={`Mais comprados`}
-                    request={getProductsByPrice}
+                    request={(i: number, p: number) =>
+                        getProductsByTopic(i, p, getMostBought)
+                    }
                     onProductClick={(product) => console.log(product)}
                 />
             </SecondPageContainer>
