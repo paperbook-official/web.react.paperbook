937e385b78f3cd9e9e4bc71c8e109bd4bd314e24
Michell Algarra <michell.algarra@gmail.com>
2021-04-08 22:39:17 -0300

    feat(utils/arrayManagement): created the range function

--- src/utils/arrayManagement.ts
+++ src/utils/arrayManagement.ts
@@ -2,3 +2,15 @@ export const getRandom = <T>(array: T[]): T => {
     const index = Math.floor(Math.random() * array.length);
     return array[index];
 };
+
+export const range = (from: number, to: number, step = 1): number[] => {
+    const range = [];
+    let i = from;
+
+    while (i <= to) {
+        range.push(i);
+        i += step;
+    }
+
+    return range;
+};
