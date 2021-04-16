5fb2b60eb3f8170eced4c17f9a9369f9fab587dc
Michell Algarra <michell.algarra@gmail.com>
2021-04-16 13:26:59 -0300

    fix(components/ProductList/styles): reversed changes made in address card branch

--- src/components/organisms/ProductList/styles.ts
+++ src/components/organisms/ProductList/styles.ts
@@ -6,7 +6,7 @@ export const Container = styled.div`
     width: 100%;
     height: auto;
     margin-top: 120px;
-    /* margin-bottom: -90px; */
+    margin-bottom: -90px;
 `;
 
 export const TopicTitle = styled.h2`
