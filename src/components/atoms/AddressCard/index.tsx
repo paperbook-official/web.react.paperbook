8eb35c09eae69a67ae302d15736e9654f74272d3
Michell Algarra <michell.algarra@gmail.com>
2021-04-17 20:51:48 -0300

    feat(components/AddressCard/index.tsx): improved the clicking methods in address card

--- src/components/atoms/AddressCard/index.tsx
+++ src/components/atoms/AddressCard/index.tsx
@@ -5,25 +5,44 @@ import { UserProxy } from '../../../models/proxies/user/user';
 
 import { useTheme } from 'styled-components';
 
+import { maskString } from '../../../utils/formatters';
+
 import { ReactComponent as CloseIcon } from '../../../assets/icons/close.svg';
 
-import { Container, ChangeAddressButton } from './styles';
+import { Container } from './styles';
 
 interface AddressCardProps {
     user: UserProxy;
     address: AddressProxy;
-    onAddressChangeClick(user: UserProxy, address: AddressProxy): void;
+    onUseAddressClick(address: AddressProxy): void;
+    onDeleteClick(address: AddressProxy): void;
 }
 
 const AddressCard: React.FC<AddressCardProps> = ({
     user,
     address,
-    onAddressChangeClick
+    onUseAddressClick,
+    onDeleteClick
 }: AddressCardProps): JSX.Element => {
     const theme = useTheme();
 
+    const handleContainerClick = (
+        event: React.MouseEvent<HTMLDivElement, MouseEvent>
+    ) => {
+        const id = (event.target as HTMLElement).id;
+
+        if (id === 'container-part') {
+            onUseAddressClick(address);
+        } else {
+            onDeleteClick(address);
+        }
+    };
+
     return (
-        <Container>
+        <Container
+            id="container-part"
+            onClick={(event) => handleContainerClick(event)}
+        >
             <div className="delete-button">
                 <CloseIcon
                     height="22"
@@ -31,18 +50,15 @@ const AddressCard: React.FC<AddressCardProps> = ({
                     color={theme.colors.defaultDarkGrey}
                 />
             </div>
-            <span className="username">{user.name}</span>
-            <span>{`${address.street}, ${address.houseNumber}`}</span>
-            <span>{address.district}</span>
-            <span>{`${address.city} - ${address.state}`}</span>
-            <span>CEP {address.cep}</span>
-            <div className="change-address-container">
-                <ChangeAddressButton
-                    onClick={() => onAddressChangeClick(user, address)}
-                >
-                    Alterar endereco de entrega
-                </ChangeAddressButton>
-            </div>
+            <span id="container-part" className="username">
+                {user.name}
+            </span>
+            <span id="container-part">{`${address.street}, ${address.houseNumber}`}</span>
+            <span id="container-part">{address.district}</span>
+            <span id="container-part">{`${address.city} - ${address.state}`}</span>
+            <span id="container-part">
+                CEP {maskString(address.cep, '#####-###')}
+            </span>
         </Container>
     );
 };
