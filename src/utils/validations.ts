13c31ab8c5b6d4a0fbbb61c29c05e1885339e6dd
Michell Algarra <michell.algarra@gmail.com>
2021-04-17 20:47:59 -0300

    feat(utils/validations): created three new validations methods for CPF, phone and number strings

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -17,3 +17,19 @@ export const cepValidation = (cep: string): boolean => {
     const regex = /[0-9]{8}/g;
     return regex.test(cep);
 };
+
+export const cpfValidation = (cpf: string): boolean => {
+    // eslint-disable-next-line no-useless-escape
+    const regex = /([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})/;
+    return regex.test(cpf);
+};
+
+export const phoneValidation = (phone: string): boolean => {
+    const regex = /^\(?(?:[14689][1-9]|2[12478]|3[1234578]|5[1345]|7[134579])\)? ?(?:[2-8]|9[1-9])[0-9]{3}-?[0-9]{4}$/;
+    return regex.test(phone);
+};
+
+export const numberValidation = (number: string): boolean => {
+    const regex = /^[1-9][0-9]*$/;
+    return regex.test(number);
+};
