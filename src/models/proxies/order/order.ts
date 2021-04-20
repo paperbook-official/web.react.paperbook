fd0e789249a411834fd0b1e8919d15784388392d
Michell Algarra <michell.algarra@gmail.com>
2021-04-19 22:05:45 -0300

    feat(proxies/order): added props to order proxy

--- src/models/proxies/order/order.ts
+++ src/models/proxies/order/order.ts
@@ -1,6 +1,7 @@
 import { OrderStatus } from '../../enums/order';
 import { ProductProxy } from '../product/product';
 import { UserProxy } from '../user/user';
+import { AddressProxy } from './../address/address';
 
 export interface OrderProxy {
     id: number;
@@ -9,10 +10,15 @@ export interface OrderProxy {
     isActive: boolean;
     status: OrderStatus;
     trackingCode: string;
+    amount: number;
+    installmentAmount: number;
+    totalPrice: number;
+    addressId: number;
     userId: number;
     productId: number;
 
     // relations
+    address?: AddressProxy;
     user?: UserProxy;
     product?: ProductProxy;
 }
