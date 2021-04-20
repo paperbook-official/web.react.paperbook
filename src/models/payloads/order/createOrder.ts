4a787f49eae400cd4307c0035b38ef93403cb89f
Michell Algarra <michell.algarra@gmail.com>
2021-04-19 22:05:15 -0300

    feat(payloads/order/createOrder): added props to create order payload

--- src/models/payloads/order/createOrder.ts
+++ src/models/payloads/order/createOrder.ts
@@ -3,6 +3,10 @@ import { OrderStatus } from '../../enums/order';
 export interface CreateOrderPayload {
     status: OrderStatus;
     trackingCode: string;
+    amount: number;
+    installmentAmount: number;
+    totalPrice: number;
+    addressId: number;
     userId: number;
     productId: number;
 }
