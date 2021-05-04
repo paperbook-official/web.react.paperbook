f6ca889a84a633f2a9e13acb75d91c02eff6dbce
Michell Algarra <michell.algarra@gmail.com>
2021-05-04 12:34:14 -0300

    feat(proxies/shoppingCart): updated shopping cart proxy

--- src/models/proxies/shoppingCart/shoppingCart.ts
+++ src/models/proxies/shoppingCart/shoppingCart.ts
@@ -1,15 +1,14 @@
-import { ProductProxy } from '../product/product';
 import { UserProxy } from '../user/user';
+import { ProductGroupProxy } from './../product/productGroup';
 
 export interface ShoppingCartProxy {
     id: number;
     createdAt: string;
     updatedAt: string;
     isActive: boolean;
-    productId: number;
     userId: number;
 
     // relations
-    product?: ProductProxy;
     user?: UserProxy;
+    productGroups?: ProductGroupProxy[];
 }
