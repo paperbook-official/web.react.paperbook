d23aee91e73de527f94ce81a36269153d041de84
Michell Algarra <michell.algarra@gmail.com>
2021-05-18 11:45:19 -0300

    feat(contexts/orderContext): removed the method to create an order and created another to get all user's orders

--- src/contexts/orderContext.tsx
+++ src/contexts/orderContext.tsx
@@ -1,16 +1,24 @@
 import React, { createContext } from 'react';
 
 import { OrderStatus } from '../models/enums/order';
-import { CreateOrderPayload } from '../models/payloads/order/createOrder';
+import { GetMany } from '../models/getMany';
 import { OrderProxy } from '../models/proxies/order/order';
 
 import api from '../services/api';
 
 import { useAuth } from '../hooks/useAuth';
 
+import { insertParamInQuery } from '../utils/formatters';
+
 export interface OrderContextData {
-    createOrder(order: CreateOrderPayload): Promise<OrderProxy>;
     updateOrderStatus(status: OrderStatus, orderId: number): Promise<void>;
+    getOrders(
+        page: number,
+        offset: number,
+        limit: number,
+        filter?: string[],
+        join?: string[]
+    ): Promise<GetMany<OrderProxy>>;
 }
 
 interface OrderProviderProps {
@@ -26,13 +34,25 @@ export const OrderProvider: React.FC<OrderProviderProps> = ({
 }: OrderProviderProps) => {
     const { token } = useAuth();
 
-    const createOrder = async (
-        order: CreateOrderPayload
-    ): Promise<OrderProxy> => {
-        const response = await api.post<OrderProxy>('/orders', order, {
-            headers: { Authorization: 'Bearer ' + token }
+    const concatParam = (url: string, param: string, value: string[]) => {
+        let fullUrl: string = url;
+        value.forEach((val, index) => {
+            if (!url.includes('?')) {
+                fullUrl += '?';
+            }
+            if (index === 0 && fullUrl[fullUrl.length - 1] !== '?') {
+                fullUrl += '&';
+            } else if (index !== 0) {
+                fullUrl += '&';
+            }
+            fullUrl += param + '=' + val;
         });
-        return response.data;
+
+        if (value.length > 0) {
+            fullUrl += '&';
+        }
+
+        return fullUrl;
     };
 
     const updateOrderStatus = async (
@@ -48,13 +68,34 @@ export const OrderProvider: React.FC<OrderProviderProps> = ({
         );
     };
 
+    const getOrders = async (
+        page: number,
+        offset: number,
+        limit: number,
+        filter: string[] = [],
+        join = ['productGroups', 'productGroups.product']
+    ): Promise<GetMany<OrderProxy>> => {
+        let url = concatParam('/users/me/orders?', 'join', join);
+        url = concatParam(url, 'filter', filter);
+
+        url = insertParamInQuery(url, 'page', page);
+        url = insertParamInQuery(url, 'offset', offset);
+        url = insertParamInQuery(url, 'limit', limit);
+        url = insertParamInQuery(url, 'sort', 'status,ASC');
+
+        const response = await api.get<GetMany<OrderProxy>>(url, {
+            headers: { Authorization: 'Bearer ' + token }
+        });
+        return response.data;
+    };
+
     return (
         //#region JSX
 
         <OrderContext.Provider
             value={{
-                createOrder,
-                updateOrderStatus
+                updateOrderStatus,
+                getOrders
             }}
         >
             {children}
