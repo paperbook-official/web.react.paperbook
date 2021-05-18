24c0986a372c147ce4730b63644d16cd4d1c6329
Michell Algarra <michell.algarra@gmail.com>
2021-05-18 12:46:08 -0300

    feat(components/Tooltip): improved the tooltip size and positioning

--- src/components/atoms/Tooltip/index.tsx
+++ src/components/atoms/Tooltip/index.tsx
@@ -14,10 +14,13 @@ const Tooltip: React.FC<TooltipProps> = ({
 
         if (tooltip) {
             if (tooltip.style.opacity !== '1') {
+                const top = event.clientY + (event.view?.pageYOffset || 0) - 30;
+
                 tooltip.style.top =
-                    event.clientY + (event.view?.pageYOffset || 0) + 30 + 'px';
+                    (event.clientY - 30 > 0 ? top : top + 50) + 'px';
+
                 tooltip.style.right =
-                    window.innerWidth - event.clientX - 20 + 'px';
+                    window.innerWidth - event.clientX - 10 + 'px';
             }
         }
     };
--- src/components/atoms/Tooltip/styles.ts
+++ src/components/atoms/Tooltip/styles.ts
@@ -4,10 +4,12 @@ export const Container = styled.div`
     position: absolute;
 
     min-width: 40px;
-    max-width: 110px;
+    max-width: 140px;
 
     padding: 2px 10px;
     z-index: 10000;
+
+    pointer-events: none;
 
     border-radius: 5px;
     background-color: ${(props) => props.theme.colors.defaultDarkGrey}99;
