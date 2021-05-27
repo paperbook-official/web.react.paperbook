3ba9ac1711cdaf9630a536bcc58bbf52839c7f3a
Michell Algarra <michell.algarra@gmail.com>
2021-05-26 22:49:36 -0300

    feat(pages/UserProducts): implemented the user products page

--- src/pages/UserProducts/index.tsx
+++ src/pages/UserProducts/index.tsx
@@ -1,29 +1,207 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import { RolesEnum } from '../../models/enums/user';
+import { ProductProxy } from '../../models/proxies/product/product';
+
+import { useLoading } from '../../hooks/useLoading';
+import { useProduct } from '../../hooks/useProduct';
+import useQuery from '../../hooks/useQuery';
+import { useUser } from '../../hooks/useUser';
 
 import FloatButton from '../../components/atoms/FloatButton';
 import Modal from '../../components/atoms/Modal';
+import Paginator from '../../components/atoms/Paginator';
+import Footer from '../../components/organisms/Footer';
 import Header from '../../components/organisms/Header';
+import ProductCard from '../../components/organisms/ProductCard';
 import ProductCreation from '../../components/organisms/ProductCreation';
+
+import { insertParamInQuery } from '../../utils/formatters';
 
 import { ReactComponent as PlusIcon } from '../../assets/icons/plus.svg';
 
-import { Container } from './styles';
+import { Container, Content, ListContainer, ResultNotFound } from './styles';
+
+const itemsPerPage = 60;
 
 const UserProducts: React.FC = () => {
+    const history = useHistory();
+    const query = useQuery();
+    const { me } = useUser();
+    const { getUserProducts } = useProduct();
+    const { isLoadingContent, setLoadingContent } = useLoading();
+
     const [isModalVisible, setModalVisible] = useState(false);
+    const [hasLoaded, setHasLoaded] = useState(false);
+
+    const [editingProduct, setEditingProduct] = useState<ProductProxy>();
+    const [products, setProducts] = useState<ProductProxy[]>([]);
+
+    const [totalMatches, setTotalMatches] = useState<number>();
+    const [pageAmount, setPageAmount] = useState(0);
+    const [page, setPage] = useState(1);
+
+    const getCurrentUrl = (): string => {
+        return history.location.pathname + history.location.search;
+    };
+
+    const initialState = async (): Promise<void> => {
+        setLoadingContent(true);
+
+        const queryPage = parseInt(query.get('page') || '1');
+
+        const response = await getUserProducts(queryPage, itemsPerPage);
+
+        if (response) {
+            setProducts(response.data);
+            setTotalMatches(response.total);
+            setPageAmount(response.pageCount);
+        }
+
+        setLoadingContent(false);
+    };
+
+    useEffect(() => {
+        if (me?.permissions === RolesEnum.COMMON) history.push('/');
+        else if (me) {
+            setHasLoaded(true);
+            initialState();
+        }
+    }, []);
+
+    useEffect(() => {
+        if (!hasLoaded) initialState();
+    }, [me]);
+
+    const handlePageChange = async (pageNumber: number): Promise<void> => {
+        setLoadingContent(true);
+
+        const origUrl = getCurrentUrl();
+        const url = insertParamInQuery(origUrl, 'page', pageNumber);
+
+        if (page !== pageNumber) {
+            history.push(url);
+            setPage(pageNumber);
+            window.scrollTo(0, 0);
+
+            const response = await getUserProducts(pageNumber, itemsPerPage);
+            if (response) setProducts(response.data);
+        }
+
+        setLoadingContent(false);
+    };
+
+    const getProductRating = (product: ProductProxy): number => {
+        if (product.ratings) {
+            const stars = product.ratings.map((rating) => rating.stars || 0);
+            const average = stars.reduce((a, b) => a + b, 0);
+            return average || 0;
+        }
+        return 0;
+    };
+
+    const onProductCreationComplete = async (
+        product: ProductProxy
+    ): Promise<void> => {
+        setModalVisible(false);
+
+        if (me) {
+            if (editingProduct) {
+                const productsList = [...products];
+
+                productsList.forEach((p, index) => {
+                    if (p.id === product.id) {
+                        productsList[index] = { ...product, user: me };
+                    }
+                });
+
+                setProducts(productsList);
+            } else {
+                setProducts([...products, { ...product, user: me }]);
+            }
+        }
+    };
 
     return (
         <Container>
             <Header />
+            <Content>
+                <div className="title-container">
+                    <h1 className="title">Seus anúncios</h1>
+                    <span className="total-matches">
+                        {totalMatches || 0} produtos anunciados
+                    </span>
+                </div>
+                <ListContainer
+                    style={{
+                        display:
+                            !isLoadingContent && totalMatches === 0
+                                ? 'flex'
+                                : 'grid'
+                    }}
+                >
+                    {!isLoadingContent && products.length > 0
+                        ? products.map((product) => (
+                              <ProductCard
+                                  style={{
+                                      width:
+                                          (window.innerWidth <= 1100 &&
+                                              window.innerWidth > 845) ||
+                                          window.innerWidth <= 665 ||
+                                          (window.innerWidth > 1200 &&
+                                              window.innerWidth <= 1366)
+                                              ? 255
+                                              : 300
+                                  }}
+                                  key={product.id}
+                                  product={product}
+                                  onClick={(p) => {
+                                      setEditingProduct(p);
+                                      setModalVisible(true);
+                                  }}
+                                  rating={getProductRating(product)}
+                              />
+                          ))
+                        : !isLoadingContent &&
+                          totalMatches === 0 && (
+                              <ResultNotFound>
+                                  Nenhum resultado encontrado!
+                              </ResultNotFound>
+                          )}
+                    {!isLoadingContent && pageAmount > 1 && (
+                        <Paginator
+                            page={page}
+                            pageAmount={pageAmount}
+                            onPageChange={handlePageChange}
+                            style={{
+                                position: 'absolute',
+                                bottom: -70,
+                                left: '50%',
+                                transform: 'translateX(-50%)'
+                            }}
+                        />
+                    )}
+                </ListContainer>
+            </Content>
+            <Footer />
             <FloatButton
                 Icon={PlusIcon}
-                onClick={() => setModalVisible(true)}
+                onClick={() => {
+                    setEditingProduct(undefined);
+                    setModalVisible(true);
+                }}
                 text="Adicionar produto"
                 iconSize={30}
             />
             {isModalVisible && (
                 <Modal onClickOutside={() => setModalVisible(false)}>
-                    <ProductCreation />
+                    <ProductCreation
+                        product={editingProduct}
+                        onComplete={(product: ProductProxy) =>
+                            onProductCreationComplete(product)
+                        }
+                    />
                 </Modal>
             )}
         </Container>
--- src/pages/UserProducts/styles.ts
+++ src/pages/UserProducts/styles.ts
@@ -1,3 +1,96 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    min-height: 100vh;
+    width: 100%;
+
+    background-color: ${(props) => props.theme.colors.background};
+
+    & div.title-container {
+        display: flex;
+        flex-direction: column;
+
+        margin-left: 10px;
+    }
+
+    & h1.title {
+        color: ${(props) => props.theme.colors.defaultDarkGrey};
+        font-size: 1.6rem;
+        font-weight: 500;
+    }
+
+    & span.total-matches {
+        color: ${(props) => props.theme.colors.defaultGrey};
+        font-size: 0.9rem;
+    }
+`;
+
+export const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+
+    margin: 40px 9%;
+    margin-bottom: 0;
+    padding-bottom: 140px;
+
+    min-height: calc(100vh - 120px);
+
+    @media (max-width: 1400px) {
+        margin: 40px 7%;
+        margin-bottom: 0;
+    }
+
+    @media (max-width: 1100px) {
+        margin: 40px 3%;
+        margin-bottom: 0;
+    }
+`;
+
+export const ListContainer = styled.div`
+    display: grid;
+    grid-column-gap: 30px;
+    grid-row-gap: 40px;
+    grid-template-columns: repeat(auto-fill, 300px);
+
+    justify-content: space-between;
+
+    position: relative;
+    flex-grow: 1;
+
+    width: 100%;
+
+    margin-top: 20px;
+
+    @media (max-width: 665px) {
+        grid-column-gap: 5px;
+        grid-template-columns: repeat(auto-fill, 255px);
+    }
+
+    @media (max-width: 1000px) {
+        grid-column-gap: 20px;
+    }
+
+    @media (max-width: 1100px) and (min-width: 845px) {
+        grid-template-columns: repeat(auto-fill, 255px);
+    }
+
+    @media (max-width: 1200px) {
+        grid-column-gap: 5px;
+    }
+
+    @media (max-width: 1366px) and (min-width: 1201px) {
+        grid-column-gap: 10px;
+        grid-template-columns: repeat(auto-fill, 255px);
+    }
+`;
+
+export const ResultNotFound = styled.span`
+    font-size: 1.3rem;
+    font-weight: 500;
+    color: ${(props) => props.theme.colors.defaultGrey};
+
+    margin-left: 50%;
+    margin-top: 10px;
+    transform: translateX(-50%);
+`;
